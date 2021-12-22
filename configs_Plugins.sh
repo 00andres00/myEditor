ca85d267b2bbec9276f3bc595121aff6e83f36eb
Andres Jacinto Cortes Garcia <90994773+and00dna@users.noreply.github.com>
2021-12-22 06:01:06 -0600

    Plugins

---
+++ configs_Plugins.sh
@@ -0,0 +1,3 @@
+git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
+git clone --depth 1 https://github.com/lifepillar/vim-mucomplete.git ~/.vim/pack/plugins/start/vim-mucomplete
