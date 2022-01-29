caf4382d7f2a210965ba21c2e3b0da0f62a609dd
and00dna <90994773+and00dna@users.noreply.github.com>
2022-01-29 02:30:17 -0600

    Modificacion de script .sh

--- configs_Plugins.sh
+++ configs_Plugins.sh
@@ -1,3 +1,18 @@
-git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
-git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
-git clone --depth 1 https://github.com/lifepillar/vim-mucomplete.git ~/.vim/pack/plugins/start/vim-mucomplete
+#!/bin/bash
+
+function setting() {
+	mv vim_settings $HOME/vimrc
+	git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
+	git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
+	git clone --depth 1 https://github.com/lifepillar/vim-mucomplete.git ~/.vim/pack/plugins/start/vim-mucomplete
+}
+
+DIR=$PWD
+cd $HOME
+
+if [ $PWD = $HOME ] 
+then
+	setting
+fi
+
+rm -rf $DIR
