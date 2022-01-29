aae156c5efba5743457309fcf8f45a7ba107c371
and00dna <90994773+and00dna@users.noreply.github.com>
2022-01-29 02:39:48 -0600

    Modificacion de script .sh

--- configs_Plugins.sh
+++ configs_Plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function setting() {
-	mv vim_settings $HOME/vimrc
+	mv $DIR/vim_settings $HOME/vimrc
 	git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
 	git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
 	git clone --depth 1 https://github.com/lifepillar/vim-mucomplete.git ~/.vim/pack/plugins/start/vim-mucomplete
