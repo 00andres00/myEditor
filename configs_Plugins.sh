f4a148d3d51b7e4fc0f229b3e63df3c9870eb8c3
root <root@flaskapp.my.domain>
2022-03-30 09:58:14 -0600

    Mejorando variable para rm

--- configs_Plugins.sh
+++ configs_Plugins.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
+#!/bin/ash
 
 function setting() {
-	mv $DIR/vim_settings $HOME/.vimrc
+	mv ./vim_settings $HOME/.vimrc
 	git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
 	git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
 	git clone --depth 1 https://github.com/lifepillar/vim-mucomplete.git ~/.vim/pack/plugins/start/vim-mucomplete
 }
 
-DIR=$PWD
-cd $HOME
+di=$(pwd)
 
-if [ $PWD = $HOME ] 
-then
-	setting
-fi
-
-rm -rf $DIR
+setting
+cd ../
+echo -e "\t\e[0;31m*** Deleting files ***"
+sleep 3
+rm -rf "./${di##*/}"
